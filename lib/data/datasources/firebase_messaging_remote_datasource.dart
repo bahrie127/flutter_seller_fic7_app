6304fb1c629b71a92c00124637cb84325137a781
bahri <saiful.bahri.tl@gmail.com>
2023-10-02 09:24:58 +0700

    integration fcm and flutter local notif

---
+++ lib/data/datasources/firebase_messaging_remote_datasource.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  FirebaseMessagingRemoteDatasource().firebaseBackgroundHandler(message);
+}
+
+class FirebaseMessagingRemoteDatasource {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotification() async {
+    await _firebaseMessaging.requestPermission();
+
+    final initializationSettingsAndroid =
+        const AndroidInitializationSettings('ic_seller');
+    final initializationSettingsIOS = DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+        onDidReceiveLocalNotification:
+            (int id, String? title, String? body, String? payload) async {
+          // showNotification(id: id, title: title, body: body, payLoad: payload);
+        });
+
+    final initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse:
+            (NotificationResponse notificationResponse) async {});
+
+    final fcmToken = await _firebaseMessaging.getToken();
+
+    print('Token: $fcmToken');
+
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message.notification?.body);
+      print(message.notification?.title);
+    });
+
+    FirebaseMessaging.onMessage.listen(firebaseBackgroundHandler);
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(firebaseBackgroundHandler);
+  }
+
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payLoad}) async {
+    return flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+            'com.example.flutter_seller_app', 'seller',
+            importance: Importance.max),
+        iOS: DarwinNotificationDetails(),
+      ),
+    );
+  }
+
+  Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
+    showNotification(
+      title: message.notification!.title,
+      body: message.notification!.body,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_seller_app/bloc/add_image/add_image_bloc.dart';
 import 'package:flutter_seller_app/bloc/bloc/add_product_bloc.dart';
 import 'package:flutter_seller_app/bloc/categories/categories_bloc.dart';
 import 'package:flutter_seller_app/bloc/products/products_bloc.dart';
+import 'package:flutter_seller_app/data/datasources/firebase_messaging_remote_datasource.dart';
 import 'package:flutter_seller_app/pages/dashboard/seller_dashboard_page.dart';
 
 import 'bloc/login/login_bloc.dart';
 import 'bloc/logout/logout_bloc.dart';
 import 'bloc/register/register_bloc.dart';
 import 'data/datasources/auth_local_datasource.dart';
+import 'firebase_options.dart';
 import 'pages/auth/auth_page.dart';
 import 'utils/light_themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessagingRemoteDatasource().initNotification();
   runApp(const MyApp());
 }
 
